f3cdd08f476935264ca76406e9188c7612404ab2
CBurton <burton.chris13@gmail.com>
2022-05-31 18:58:08 -0500

    created user and post table in database

---
+++ models/index.js
@@ -0,0 +1,5 @@
+// import models
+const User = require('./user');
+const Post = require('./post');
+
+module.exports = { User, Post };
---
+++ models/post.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+// create our Post model
+class Post extends Model {
+  // set up method to run on instance data (per user) to check password
+  
+}
+
+// create fields/columns for Post model
+Post.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      content: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'user',
+          key: 'id'
+        }
+      }
+    },
+    {
+      sequelize,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'post'
+    }
+  );
+  
+  module.exports = Post;
