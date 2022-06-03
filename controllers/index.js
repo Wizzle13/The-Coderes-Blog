19d24fbb484ac9a9c370dc042bd9aec12266b41f
CBurton <burton.chris13@gmail.com>
2022-06-02 21:06:52 -0500

    Created login and sign up pages

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const apiRoutes = require('./api');
+router.use('/api', apiRoutes);
 
 router.get ('/', (req,res) => {
     res.render('homepage');
@@ -8,6 +10,9 @@ router.get('/login', (req, res) => {
 })
 router.get('/signup', (req, res) => {
     res.render('signup');
+})
+router.get('/dashboard', (req, res) => {
+    res.render('dashboard');
 })
 router.use((req, res) => {
     res.status(404).end();
