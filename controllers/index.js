1ca1f541713aa75a75840f2f49ff169c5c5c21b5
CBurton <burton.chris13@gmail.com>
2022-06-02 20:25:49 -0500

    basic layout

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
 
-
-
+router.get ('/', (req,res) => {
+    res.render('homepage');
+})
+router.get('/login', (req, res) => {
+    res.render('login');
+})
+router.get('/signup', (req, res) => {
+    res.render('signup');
+})
 router.use((req, res) => {
     res.status(404).end();
 });
