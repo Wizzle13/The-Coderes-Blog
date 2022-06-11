f464cf286d93eff56bcbaabd8a147f34da41ba2d
CBurton <burton.chris13@gmail.com>
2022-06-11 11:09:24 -0500

    created post section to post content

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -64,7 +64,7 @@ router.post('/', (req, res) => {
       req.session.username = dbUserData.username;
       req.session.loggedIn = true;
   
-      res.json(dbUserData);
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
     });
   });   
 });
